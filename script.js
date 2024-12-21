334a48d38ec458ccebfb8b3a1db56463ef74048e
yassine <yassinelamachi16@gmail.com>
2024-12-21 21:23:08 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("surpriseButton").addEventListener("click", function () {
+    const surprise = document.getElementById("surprise");
+    surprise.classList.toggle("hidden");
+});
